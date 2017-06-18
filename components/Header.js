1cc5b86278e613b94a49ef6c532e0e656be8f018
Mac Cowell <mac@has100ideas.com>
2017-06-17 21:22:47 -0700

    bootstrap+jquery-js, no css tho

--- components/Header.js
+++ components/Header.js
@@ -1,88 +1,92 @@
+// import BootstrapMin from './bootstrap-min.css.js'
+
 export default () => (
 
   <header className="sans-serif">
 
-  <style jsx>{`
-    header {
-      width: 100%;
-      height: 76px;
-      position: fixed;
-      top: 0;
-      left: 0;
-      z-index: 100;
-      background: rgba(3, 53, 132, .5);
-    }
-    #logo {
-      margin: 20px;
-      float: left;
-      width: 200px;
-      height: 40px;
-      background: url(/static/logo.png) no-repeat center;
-      display: block;
-    }
-    #menu-icon {
-      display: none;
-      width: 40px;
-      height: 40px;
-      background: #4C8FEC url(/static/menu-icon.png) center;
-    }
-    nav {
-      float: right;
-      padding: 20px;
-    }
-    nav ul {
-      margin-top: 0;
-      margin-bottom: 10px;
-      list-style: none;
-    }
-    nav ul li {
-        display: inline-block;
-        float: left;
-        padding: 10px;
-    }
-    nav a {
-      color: white;
-      text-decoration: none;
-      font-weight: bold;
-      transition: all 200ms ease-in;
-      font-size: 14px;
-      font-family: 'Lato' , Helvetica, Arial, sans-serif;
-      line-height: 20px;
-      transition: all 200ms ease-in;
-    }
-    nav a:hover {
-      color: grey;
-      text-decoration: none;
-    }
-    @media only screen and (max-width : 640px) {
+    {/* {BootstrapMin} */}
+
+    <style jsx>{`
       header {
-        position: absolute;
-      }
-      #menu-icon {
-        display:inline-block;
-      }
-      nav ul, nav:active ul {
-        display: none;
-        position: absolute;
-        padding: 20px;
-        background: #4C8FEC;
-        border: 5px solid #4C8FEC;
-        right: 20px;
-        top: 60px;
-        width: 50%;
-        border-radius: 4px 0 4px 4px;
-      }
-      nav li {
-        text-align: center;
         width: 100%;
-        padding: 10px 0;
-        margin: 0;
+        height: 76px;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 100;
+        background: rgba(3, 53, 132, .5);
       }
-      nav:hover ul {
+      #logo {
+        margin: 20px;
+        float: left;
+        width: 200px;
+        height: 40px;
+        background: url(/static/logo.png) no-repeat center;
         display: block;
       }
-    }
-  `}</style>
+      #menu-icon {
+        display: none;
+        width: 40px;
+        height: 40px;
+        background: #4C8FEC url(/static/menu-icon.png) center;
+      }
+      nav {
+        float: right;
+        padding: 20px;
+      }
+      nav ul {
+        margin-top: 0;
+        margin-bottom: 10px;
+        list-style: none;
+      }
+      nav ul li {
+          display: inline-block;
+          float: left;
+          padding: 10px;
+      }
+      nav a {
+        color: white;
+        text-decoration: none;
+        font-weight: bold;
+        transition: all 200ms ease-in;
+        font-size: 14px;
+        font-family: 'Lato' , Helvetica, Arial, sans-serif;
+        line-height: 20px;
+        transition: all 200ms ease-in;
+      }
+      nav a:hover {
+        color: grey;
+        text-decoration: none;
+      }
+      @media only screen and (max-width : 640px) {
+        header {
+          position: absolute;
+        }
+        #menu-icon {
+          display:inline-block;
+        }
+        nav ul, nav:active ul {
+          display: none;
+          position: absolute;
+          padding: 20px;
+          background: #4C8FEC;
+          border: 5px solid #4C8FEC;
+          right: 20px;
+          top: 60px;
+          width: 50%;
+          border-radius: 4px 0 4px 4px;
+        }
+        nav li {
+          text-align: center;
+          width: 100%;
+          padding: 10px 0;
+          margin: 0;
+        }
+        nav:hover ul {
+          display: block;
+        }
+      }
+    `}</style>
 
     <a href="/" className="grow" id="logo"></a>
 
--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,11 @@ export default class Document extends NextDocument {
     return (
       <html>
         <Head>
-          {/* <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" /> */}
-          <script src="/static/jquery.min.js"></script>
-          <link rel="stylesheet" href="/static/bootstrap.min.css" />
+          <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+          <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+          {/* <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" /> */}
+          {/* <script src="/static/jquery.min.js"></script>
+          <link rel="stylesheet" href="/static/bootstrap.min.css" /> */}
           <meta name='viewport' content='width=device-width, initial-scale=1'/>
           <meta content="Homepage" name="description" />
           <meta content="website" property="og:type" />
